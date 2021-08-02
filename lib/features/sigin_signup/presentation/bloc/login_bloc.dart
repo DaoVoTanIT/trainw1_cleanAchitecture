0dbc52bb228b54d4378244832cfba9957d0cde93
DaoVoTanIT <daovotan3@gmail.com>
2021-08-02 22:42:21 +0700

    [bloc] update bloc

--- lib/features/sigin_signup/presentation/bloc/login_bloc.dart
+++ lib/features/sigin_signup/presentation/bloc/login_bloc.dart
@@ -3,16 +3,32 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import 'package:clean_achitecture/features/sigin_signup/domain/entities/loginModel.dart';
+import 'package:clean_achitecture/features/sigin_signup/domain/usecases/login.dart';
+import 'package:clean_achitecture/features/sigin_signup/presentation/dto/login_dto.dart';
+
 part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginInitial());
-
+  LoginBloc(
+    this.loginUseCase,
+  ) : super(LoginInitial());
+  final LoginUseCase loginUseCase;
   @override
   Stream<LoginState> mapEventToState(
     LoginEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is CheckLoginEvent) {
+      yield CheckLoginState();
+      final result = await loginUseCase.call(
+        Params(
+          login: LoginModel(
+              password: event.login.password, login: event.login.username),
+        ),
+      );
+      yield result.fold((l) => ErrorLoggedState(),
+          (r) => (r) ? LoggedState() : ErrorLoggedState());
+    }
   }
 }
