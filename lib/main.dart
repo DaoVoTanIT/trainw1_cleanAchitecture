ba06dd129609cc093c50c8fc606e9bab3372e23e
DaoVoTanIT <daovotan3@gmail.com>
2021-08-31 11:13:08 +0700

    change function main

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,38 @@
-
 import 'package:clean_achitecture/features/sigin_signup/presentation/screen/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'injection_container.dart' as di;
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-        // home: ChangeNotifierProvider(
-        //   create: (context) => AuthService(),
-        //   child: StreamBuilder(
-        //     stream: FirebaseAuth.instance.authStateChanges(),
-        //     builder: (context, snapshot) {
-        //       final provider = Provider.of<AuthService>(context);
-        //       if (provider.isSigningIn) {
-        //         return buildLoading();
-        //       } else if (snapshot.hasData) {
-        //         return HomeScreen();
-        //       } else {
-        //         //return SigninOrSignupScreen();
-        //         return SignIn();
-        //       }
-        //     },
-        //   ),
-        // ),
-        home: SignIn(),
-      );
-  Widget buildLoading() => Stack(
-        fit: StackFit.expand,
-        children: [
-          Center(child: CircularProgressIndicator()),
-        ],
-      );
-}
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   await di.init();
+//   runApp(MyApp());
+// }
 
 // class MyApp extends StatelessWidget {
-//   final _init = Firebase.initializeApp();
 //   @override
-//   Widget build(BuildContext context) {
-//     // return FutureBuilder(
-//     //     future: _init,
-//     //     builder: (BuildContext context, snapshot) {
-
-//     //       final provider = Provider.of<AuthService>(context);
-//     //       if (provider.isSigningIn) {
-//     //         return buildLoading();
-//     //       } else if (snapshot.hasData) {
-//     //         return HomeScreen();
-//     //       } else {
-//     //         return Authenticate();
-//     //       }
-//     //     });
-//     return Scaffold(body: SignIn());
-//   }
-
+//   Widget build(BuildContext context) => MaterialApp(
+//         // home: ChangeNotifierProvider(
+//         //   create: (context) => AuthService(),
+//         //   child: StreamBuilder(
+//         //     stream: FirebaseAuth.instance.authStateChanges(),
+//         //     builder: (context, snapshot) {
+//         //       final provider = Provider.of<AuthService>(context);
+//         //       if (provider.isSigningIn) {
+//         //         return buildLoading();
+//         //       } else if (snapshot.hasData) {
+//         //         return HomeScreen();
+//         //       } else {
+//         //         //return SigninOrSignupScreen();
+//         //         return SignIn();
+//         //       }
+//         //     },
+//         //   ),
+//         // ),
+//         home: SignIn(),
+//       );
 //   Widget buildLoading() => Stack(
 //         fit: StackFit.expand,
 //         children: [
@@ -68,3 +40,27 @@ class MyApp extends StatelessWidget {
 //         ],
 //       );
 // }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SignIn(),
+    );
+  }
+}
