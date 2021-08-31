3630bdb93934becd8fc9d3ca3c4fa4aa4b8d267d
DaoVoTanIT <daovotan3@gmail.com>
2021-08-31 11:14:58 +0700

    create file Loading_widget

---
+++ lib/features/sigin_signup/presentation/widget/Loading_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoadingWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height / 3,
+              child: Center(
+                child: SingleChildScrollView(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
