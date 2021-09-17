80b12e4549d897c4ed50d1b30d14256a3f004269
DaoVoTanIT <daovotan3@gmail.com>
2021-09-17 22:47:39 +0700

    add data

---
+++ lib/features/room/data/repositories/data.dart
@@ -0,0 +1,10 @@
+import 'package:clean_achitecture/features/account/presentation/model/inforRoom.dart';
+
+List<InforRoom> inforRoom = [
+  InforRoom(
+      id: "0",
+      address: "283, Le van viet",
+      imageUrl:
+          "https://c1.wallpaperflare.com/preview/575/637/231/fashion-style-boys-fashion-pose.jpg",
+      roomRates: 293939),
+];
