5c4caaadc156e4100f5901fa153cb86f93b3a692
DaoVoTanIT <daovotan3@gmail.com>
2021-09-17 22:45:15 +0700

    add route

--- lib/routes/route_name.dart
+++ lib/routes/route_name.dart
@@ -1,4 +1,9 @@
 class RouteName {
+  static final String splashPage = 'SplashPage';
   static final String loginPage = 'LoginPage';
+  static final String signupPage = 'SignUpPage';
   static final String homePage = 'HomePage';
+  static final String searchMapPage = 'MapPage';
+  static final String savedRoomPage = 'SavedRoomPage';
+  static final String profilePage = 'ProfilePage';
 }
--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,14 +1,29 @@
-import 'package:clean_achitecture/features/Home/presentation/page/HomeScreen.dart';
+import 'package:clean_achitecture/features/Map/presentation/page/SearchMap.dart';
+import 'package:clean_achitecture/features/account/presentation/page/account.dart';
+import 'package:clean_achitecture/features/room/presentation/page/Room.dart';
+import 'package:clean_achitecture/features/saveRoom/presentation/page/savedRoom.dart';
 import 'package:clean_achitecture/features/sigin_signup/presentation/screen/login.dart';
+import 'package:clean_achitecture/features/sigin_signup/presentation/screen/signup.dart';
+import 'package:clean_achitecture/splash/presentation/page/splash.dart';
 import 'package:flutter/material.dart';
 
 class Routers {
   static Route generateRoute(RouteSettings settings) {
     switch (settings.name) {
+      case 'SplashPage':
+        return MaterialPageRoute(builder: (context) => SplashPage());
       case 'LoginPage':
-        return MaterialPageRoute(builder: (context) => LoginPage());
+        return MaterialPageRoute(builder: (context) => SigninPage());
+      case 'SignUpPage':
+        return MaterialPageRoute(builder: (context) => SignUpPage());
       case 'HomePage':
-        return MaterialPageRoute(builder: (context) => HomePage());
+        return MaterialPageRoute(builder: (context) => Room());
+      case 'MapPage':
+        return MaterialPageRoute(builder: (context) => SearchMap());
+      case 'SavedRoomPage':
+        return MaterialPageRoute(builder: (context) => SavedRoom());
+      case 'ProfilePage':
+        return MaterialPageRoute(builder: (context) => ProfilePage());
       default:
         {
           return MaterialPageRoute(builder: (_) {
