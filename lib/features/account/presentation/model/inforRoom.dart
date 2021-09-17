a65db01b5ae29e3005032507ecdf7a7ffa5c4e0e
DaoVoTanIT <daovotan3@gmail.com>
2021-09-17 22:48:44 +0700

    add model

---
+++ lib/features/account/presentation/model/inforRoom.dart
@@ -0,0 +1,12 @@
+class InforRoom {
+  final String id;
+  final String address;
+  final String imageUrl;
+  final int roomRates;
+  InforRoom({
+    required this.id,
+    required this.address,
+    required this.imageUrl,
+    required this.roomRates,
+  });
+}
