9e395aca0f0f8c1a1aa28f7a968926135a641b1a
DaoVoTanIT <daovotan3@gmail.com>
2021-09-26 22:57:58 +0700

    refactor(confix_ios): Confix map Configure the map in ios to

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,13 @@
 import UIKit
 import Flutter
-
+import GoogleMaps
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCj-iZK-3iRIK1rkdi-ayaAw7Np9HNN9GE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
