aeeb684657673141ab9beea9c8d806d6ff434657
DaoVoTanIT <daovotan3@gmail.com>
2021-09-26 17:23:01 +0700

    change appBar in profile

--- lib/features/account/presentation/page/account.dart
+++ lib/features/account/presentation/page/account.dart
@@ -1,4 +1,7 @@
+import 'package:clean_achitecture/Theme/color.dart';
+import 'package:clean_achitecture/style/styleAppBar.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -11,8 +14,46 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tài Khoản'),
+      appBar: StyleAppBar(
+        backgroundColor: kBackgroudColor,
+        elevation: 0.3,
+        height: 60,
+        leading: InkWell(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsetsDirectional.only(start: 5),
+            child: ShaderMask(
+                child: Icon(
+                  FontAwesomeIcons.heartBroken,
+                  size: 30,
+                  color: Colors.redAccent,
+                ),
+                blendMode: BlendMode.srcATop,
+                shaderCallback: (bounds) {
+                  return LinearGradient(
+                          colors: [
+                        Colors.redAccent,
+                        Colors.deepPurpleAccent,
+                      ],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          tileMode: TileMode.repeated)
+                      .createShader(bounds);
+                }),
+          ),
+        ),
+        title: Text(
+          'Tài khoản',
+          style: TextStyle(fontSize: 20.0, color: Colors.blue),
+        ),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.settings),
+              color: Colors.grey,
+              onPressed: () {
+                Navigator.of(context).pushNamed('/notification');
+              })
+        ],
       ),
       body: Center(
         child: Text('Tai Khoan'),
