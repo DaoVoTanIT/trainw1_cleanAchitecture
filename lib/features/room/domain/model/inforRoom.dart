3784fe6630d1a08f9490cacfb0aa7179e7c6277d
DaoVoTanIT <daovotan3@gmail.com>
2021-09-26 17:21:39 +0700

    create model Room

---
+++ lib/features/room/domain/model/inforRoom.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+class Room {
+  final String id;
+  final String address;
+  final String imageUrl;
+  final double roomRates; //gia phong
+  final int area;
+  Room({
+    required this.id,
+    required this.address,
+    required this.imageUrl,
+    required this.roomRates,
+    required this.area,
+  });
+}
