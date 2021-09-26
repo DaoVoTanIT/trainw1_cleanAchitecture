97fc91a289b4b6ac0fe5680304206a547992ed51
DaoVoTanIT <daovotan3@gmail.com>
2021-09-26 17:21:56 +0700

    implement model Room

--- lib/features/room/data/repositories/data.dart
+++ lib/features/room/data/repositories/data.dart
@@ -1,10 +1,76 @@
-import 'package:clean_achitecture/features/account/presentation/model/inforRoom.dart';
+import 'package:clean_achitecture/features/room/domain/model/inforRoom.dart';
 
-List<InforRoom> inforRoom = [
-  InforRoom(
+List<Room> inforRoom = [
+  Room(
       id: "0",
       address: "283, Le van viet",
-      imageUrl:
-          "https://c1.wallpaperflare.com/preview/575/637/231/fashion-style-boys-fashion-pose.jpg",
-      roomRates: 293939),
+      imageUrl: "assets/images/room1.jpeg",
+      roomRates: 3.2,
+      area: 12),
+  Room(
+      id: "1",
+      address: "739, Vo Van Ngan",
+      imageUrl: "assets/images/room2.jpeg",
+      roomRates: 4,
+      area: 32),
+  Room(
+      id: "2",
+      address: "739, Vo Van Ngan",
+      imageUrl: "assets/images/room3.jpeg",
+      roomRates: 4.0,
+      area: 23),
+  Room(
+      id: "3",
+      address: "739, Vo Van Ngan",
+      imageUrl: "assets/images/room4.jpeg",
+      roomRates: 1.5,
+      area: 14),
+  Room(
+      id: "0",
+      address: "283, Le van viet",
+      imageUrl: "assets/images/room1.jpeg",
+      roomRates: 3.2,
+      area: 12),
+  Room(
+      id: "1",
+      address: "739, Vo Van Ngan",
+      imageUrl: "assets/images/room2.jpeg",
+      roomRates: 4,
+      area: 32),
+  Room(
+      id: "2",
+      address: "739, Vo Van Ngan",
+      imageUrl: "assets/images/room3.jpeg",
+      roomRates: 4.0,
+      area: 23),
+  Room(
+      id: "3",
+      address: "739, Vo Van Ngan",
+      imageUrl: "assets/images/room4.jpeg",
+      roomRates: 1.5,
+      area: 14),
+  Room(
+      id: "0",
+      address: "283, Le van viet",
+      imageUrl: "assets/images/room1.jpeg",
+      roomRates: 3.2,
+      area: 12),
+  Room(
+      id: "1",
+      address: "739, Vo Van Ngan",
+      imageUrl: "assets/images/room2.jpeg",
+      roomRates: 4,
+      area: 32),
+  Room(
+      id: "2",
+      address: "739, Vo Van Ngan",
+      imageUrl: "assets/images/room3.jpeg",
+      roomRates: 4.0,
+      area: 23),
+  Room(
+      id: "3",
+      address: "739, Vo Van Ngan",
+      imageUrl: "assets/images/room4.jpeg",
+      roomRates: 1.5,
+      area: 14),
 ];
