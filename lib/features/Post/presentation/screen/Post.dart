fb4871ed00aae0488d9d66ff77001b020c7f022a
DaoVoTanIT <daovotan3@gmail.com>
2021-09-30 22:36:38 +0700

    feat: create Post room Create a post including room type - price - area

--- lib/features/Post/presentation/screen/Post.dart
+++ lib/features/Post/presentation/screen/Post.dart
@@ -1,7 +1,9 @@
 import 'package:clean_achitecture/Theme/color.dart';
+import 'package:clean_achitecture/features/Post/presentation/widget/UtilitiesRoom_Infor.dart';
 import 'package:clean_achitecture/style/styleAppBar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_toggle_tab/flutter_toggle_tab.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class PostPage extends StatefulWidget {
@@ -11,7 +13,15 @@ class PostPage extends StatefulWidget {
 
 class _PostPageState extends State<PostPage> {
   int activeStepIndex = 0;
-
+  bool isSelectedWifi = false;
+  bool isSelectedWC = false;
+  bool isSelectedMB = false;
+  bool isSelectedClock = false;
+  bool isSelectedAirFresher = false;
+  bool isSelectedCook = false;
+  bool isSelectedFridge = false;
+  bool isSelectedWM = false;
+  int counter = 0;
   List<Step> stepList() => [
         Step(
           state: activeStepIndex <= 0 ? StepState.editing : StepState.complete,
@@ -82,15 +92,206 @@ class _PostPageState extends State<PostPage> {
             title: Text('Thông tin'),
             content: SingleChildScrollView(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Email Address'),
+                  FlutterToggleTab(
+                    width: 75,
+                    borderRadius: 30,
+                    height: 40,
+                    selectedTextStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700),
+                    unSelectedTextStyle: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500),
+                    labels: ["Phòng", "Căn hộ", "Nguyên căn"],
+                    selectedLabelIndex: (index) {
+                      setState(() {
+                        counter = index;
+                      });
+                      print("Selected Index $index");
+                    },
+                    selectedIndex: counter,
                   ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Gía phòng(VND)', style: TextStyle(fontSize: 16)),
+                        Text('Diện tích(m2)', style: TextStyle(fontSize: 16)),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Row(
+                      children: <Widget>[
+                        Flexible(
+                            child: Container(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                                hintText: "3.000.000",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10))),
+                          ),
+                        )),
+                        SizedBox(width: 10),
+                        Flexible(
+                            child: Container(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                                hintText: "20",
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10))),
+                          ),
+                        ))
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child:
+                        Text('Tiện ích phòng', style: TextStyle(fontSize: 16)),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isSelectedWifi = !isSelectedWifi;
+                              });
+                            },
+                            icon: Icon(FontAwesomeIcons.wifi,
+                                color:
+                                    isSelectedWifi ? Colors.blue : Colors.grey),
+                            //color: isSelected? Colors.blue : Colors.grey
+                          ),
+                          Text('Wifi')
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isSelectedWC = !isSelectedWC;
+                              });
+                            },
+                            icon: Icon(FontAwesomeIcons.toilet),
+                            color: isSelectedWC ? Colors.blue : Colors.grey,
+                          ),
+                          Text('WC riêng')
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isSelectedMB = !isSelectedMB;
+                              });
+                            },
+                            icon: Icon(FontAwesomeIcons.motorcycle),
+                            color: isSelectedMB ? Colors.blue : Colors.grey,
+                          ),
+                          Text('Giữ xe')
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isSelectedClock = !isSelectedClock;
+                              });
+                            },
+                            icon: Icon(Icons.access_alarm),
+                            color: isSelectedClock ? Colors.blue : Colors.grey,
+                          ),
+                          Text(
+                            'Tự do',
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isSelectedAirFresher = !isSelectedAirFresher;
+                              });
+                            },
+                            icon: Icon(FontAwesomeIcons.fan),
+                            color: isSelectedAirFresher
+                                ? Colors.blue
+                                : Colors.grey,
+                          ),
+                          Text('Điều hoà')
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isSelectedCook = !isSelectedCook;
+                              });
+                            },
+                            icon: Icon(FontAwesomeIcons.conciergeBell),
+                            color: isSelectedCook ? Colors.blue : Colors.grey,
+                          ),
+                          Text('Bếp')
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isSelectedFridge = !isSelectedFridge;
+                              });
+                            },
+                            icon: Icon(FontAwesomeIcons.windowMaximize),
+                            color: isSelectedFridge ? Colors.blue : Colors.grey,
+                          ),
+                          Text('Tủ lạnh')
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isSelectedWM = !isSelectedWM;
+                              });
+                            },
+                            icon: Icon(FontAwesomeIcons.algolia),
+                            color: isSelectedWM ? Colors.blue : Colors.grey,
+                          ),
+                          Text(
+                            'Máy giặc',
+                          )
+                        ],
+                      ),
+                    ],
+                  )
                 ],
               ),
             )),
         Step(
-          state: StepState.complete,
+          state: StepState.editing,
           isActive: activeStepIndex >= 2,
           title: Text('Xác nhận'),
           content: Column(
@@ -105,65 +306,64 @@ class _PostPageState extends State<PostPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: StyleAppBar(
-        backgroundColor: kBackgroudColor,
-        elevation: 0.3,
-        height: 60,
-        leading: InkWell(
-          onTap: () {},
-          child: Container(
-            margin: EdgeInsetsDirectional.only(start: 5),
-            child: ShaderMask(
-                child: Icon(
-                  FontAwesomeIcons.heartBroken,
-                  size: 42,
-                  color: Colors.redAccent,
-                ),
-                blendMode: BlendMode.srcATop,
-                shaderCallback: (bounds) {
-                  return LinearGradient(
-                          colors: [
-                        Colors.redAccent,
-                        Colors.deepPurpleAccent,
-                      ],
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
-                          tileMode: TileMode.repeated)
-                      .createShader(bounds);
-                }),
+        appBar: StyleAppBar(
+          backgroundColor: kBackgroudColor,
+          elevation: 0.3,
+          height: 60,
+          leading: InkWell(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsetsDirectional.only(start: 5),
+              child: ShaderMask(
+                  child: Icon(
+                    FontAwesomeIcons.heartBroken,
+                    size: 42,
+                    color: Colors.redAccent,
+                  ),
+                  blendMode: BlendMode.srcATop,
+                  shaderCallback: (bounds) {
+                    return LinearGradient(
+                            colors: [
+                          Colors.redAccent,
+                          Colors.deepPurpleAccent,
+                        ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            tileMode: TileMode.repeated)
+                        .createShader(bounds);
+                  }),
+            ),
           ),
+          title: Text(
+            'Đăng tin',
+            style: TextStyle(fontSize: 20.0, color: Colors.blue),
+          ),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.settings),
+                color: Colors.grey,
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/notification');
+                })
+          ],
         ),
-        title: Text(
-          'Đăng tin',
-          style: TextStyle(fontSize: 20.0, color: Colors.blue),
-        ),
-        actions: [
-          IconButton(
-              icon: Icon(Icons.settings),
-              color: Colors.grey,
-              onPressed: () {
-                Navigator.of(context).pushNamed('/notification');
-              })
-        ],
-      ),
-      body: Stepper(
-        //type: StepperType.horizontal,
-        currentStep: activeStepIndex,
-        steps: stepList(),
-        onStepContinue: () {
-          if (activeStepIndex < (stepList().length)) {
-            activeStepIndex += 1;
-          }
-          setState(() {});
-        },
-        onStepCancel: () {
-          if (activeStepIndex == 0) {
-            return;
-          }
-          activeStepIndex -= 1;
-          setState(() {});
-        },
-      ),
-    );
+        body: Stepper(
+          type: StepperType.horizontal,
+          currentStep: activeStepIndex,
+          steps: stepList(),
+          onStepContinue: () {
+            if (activeStepIndex < (stepList().length)) {
+              activeStepIndex += 1;
+            }
+            setState(() {});
+          },
+          onStepCancel: () {
+            if (activeStepIndex == 0) {
+              return;
+            }
+            activeStepIndex -= 1;
+            setState(() {});
+          },
+        ));
   }
 }
