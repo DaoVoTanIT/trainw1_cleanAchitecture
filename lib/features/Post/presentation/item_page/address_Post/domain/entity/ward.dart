6c2f49588d849301811b26a50ec4ea6c716d7ce3
DaoVoTanIT <daovotan3@gmail.com>
2021-11-13 17:10:52 +0700

    feat: create Ward class

---
+++ lib/features/Post/presentation/item_page/address_Post/domain/entity/ward.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+class Ward {
+  Ward({
+    required this.idWard,
+    required this.name,
+    required this.type,
+    required this.idDistrict,
+  });
+
+  String idWard;
+  String name;
+  Type type;
+  String idDistrict;
+  // static Ward fromJson(json) => Ward(
+  //       idWard: json['id_Ward'],
+  //       name: json['name'],
+  //       type: json['type'],
+  //       idDistrict: json['id_District'],
+  //     );
+  static Ward fromJson(json) => Ward(
+        idWard: json['id_District'],
+        name: json['name'],
+        type: json['type'],
+        idDistrict: json['id_District'],
+      );
+}
+
+enum Type { PHNG, TH_TRN, X }
